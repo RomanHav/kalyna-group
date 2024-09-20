631b5d3117bb58164527d6d99d8c5180b5327265
RomanHav <roman14082004@gmail.com>
2024-09-20 03:19:00 +0300

    ahaha useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import NavBar from './components/NavBar';
 import HeaderVideo from './components/HeaderVideo';
