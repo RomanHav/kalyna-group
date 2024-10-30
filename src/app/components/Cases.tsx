7717d9ef222c600376f3921d941c3a8b49d0dc31
RomanHav <roman14082004@gmail.com>
2024-10-30 16:26:55 +0200

    finish cases

--- src/app/components/Cases.tsx
+++ src/app/components/Cases.tsx
@@ -19,11 +19,11 @@ const Cases: React.FC<CasesProps> = ({ info }) => {
   return (
     <div className="flex flex-col my-8 px-[45px]  relative gap-10">
       <Title id="cases" title="Cases" />
-      <ul className="flex flex-col items-center gap-9">
+      <ul className="flex flex-col items-center gap-9 md:px-16">
         {info.map(infopart => (
           <li
             key={nanoid()}
-            className={`${infopart.color} flex w-full items-center rounded-3xl py-3 px-5`}
+            className={`${infopart.color} flex w-full items-center rounded-3xl py-3 md:py-4 px-5`}
           >
             <CasesPart
               span={infopart.span}
