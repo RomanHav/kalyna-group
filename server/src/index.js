9c13cec2cab09a0d8604661fb1730473f1a455de
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-11-24 08:57:17 +0100

    update server

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+import { serverKalyna } from './serverKalyna.js';
+import  {initMongoDB } from './db/initMongoDB.js';
+
+const bootstrap = async () => {
+  await initMongoDB();
+  serverKalyna();
+};
+
+bootstrap();
