4dafeabc4bad43512448d8d28703774251432f1e
sashakter <62474613+sashakter@users.noreply.github.com>
2024-11-28 13:04:36 +0100

    Corrections/part1 (#28)
    * update layout.tsx
    
    * update layout.tsx
    
    * update server
    
    * update server
    
    * update server
    
    * update server with changes
    
    * update server with changes 2
    
    * update form
    
    * update prices, HeaderVideo.tsx, HeaderVideo.module.css
    
    * fix ContactUsModal.tsx, add links, fix subscribe email section
    
    ---------
    
    Co-authored-by: sashakter <149799726+alexsnowll@users.noreply.github.com>

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import { IoLogoInstagram } from 'react-icons/io5';
 import { FaLinkedin, FaBehance } from 'react-icons/fa';
 import { useId } from 'react';
 import css from './Footer.module.css';
-import dynamic from 'next/dynamic';
+import CalendlyEmbed from '@/app/components/CalendlyEmbed';
 
 const Footer = () => {
   const [email, setEmail] = useState('');
@@ -22,13 +22,9 @@ const Footer = () => {
     console.log({ email });
   };
 
-  const CalendlyEmbed = dynamic(() => import('../CalendlyEmbed'), {
-    ssr: false,
-  });
-
   useEffect(() => {
     const handleResize = () => setIsDesktop(window.innerWidth >= 769);
-    handleResize(); // Set initial state
+    handleResize();
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
@@ -52,9 +48,8 @@ const Footer = () => {
                   >
                     Subscribe now for news and new services
                   </label>
-                  <div className="flex w-full md:max-w-full gap-4">
                     <input
-                      className="flex-grow pl-2 py-2 rounded-xl bg-inherit border border-white"
+                      className="pl-2 py-2 max-w-[80%] md:max-w-full rounded-xl bg-inherit border border-white"
                       id={`${id}-email`}
                       type="email"
                       value={email}
@@ -67,16 +62,16 @@ const Footer = () => {
                         <span className={css.buttonText}>Subscribe</span>
                       </button>
                     )}
-                  </div>
+
+                  {!isDesktop && (
+                    <button
+                      className="relative z-[5] bg-gradient-to-r from-custom-blue to-custom-green text-white py-2 px-6 md:px-8 rounded-2xl uppercase text-[18px] font-semibold flex"
+                      type="submit"
+                    >
+                      Subscribe
+                    </button>
+                  )}
                 </div>
-                {!isDesktop && (
-                  <button
-                    className="relative z-[5] bg-gradient-to-r from-custom-blue to-custom-green text-white py-2 px-6 md:px-8 rounded-2xl uppercase text-[18px] font-semibold flex"
-                    type="submit"
-                  >
-                    Subscribe
-                  </button>
-                )}
               </div>
             </form>
           </div>
@@ -143,7 +138,9 @@ const Footer = () => {
                 <span className="text-3xl uppercase font-medium">
                   Get in touch
                 </span>
-                <CalendlyEmbed url={'https://calendly.com/kalynaitgroup/30min'}/>
+                <CalendlyEmbed
+                  url={'https://calendly.com/kalynaitgroup/30min'}
+                />
                 <div className="relative">
                   <a
                     className="text-xl font-light cursor-pointer after:absolute after:-bottom-2 after:left-0 after:h-[1px] after:w-0 after:bg-white hover:after:w-full hover:after:duration-200"
@@ -154,13 +151,13 @@ const Footer = () => {
                 </div>
               </div>
               <div className="flex gap-11 justify-center items-center">
-                <a>
+                <a href="https://www.instagram.com/kalynaitgroup">
                   <IoLogoInstagram className="fill-white" size={42} />
                 </a>
-                <a>
+                <a href="https://www.linkedin.com/company/kalyna-group/">
                   <FaLinkedin className="fill-white" size={42} />
                 </a>
-                <a>
+                <a href="#">
                   <FaBehance className="fill-white" size={42} />
                 </a>
               </div>
