f750ab8cbdf19965af521d380ca3134219c5e4fd
Roman_Havrylov <146737481+RomanHav@users.noreply.github.com>
2024-11-30 21:55:02 +0200

    Developer/add google tag (#30)
    * add favicon
    
    * add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import React from 'react';
 import { GoogleAnalytics } from '@next/third-parties/google';
@@ -22,29 +23,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="description"
-          content="Create an Effective Website for Your Business With Us"
-        />
-        <link rel="icon" href="./favicon.ico" />
-        <link rel="apple-touch-icon" href="./apple-icon.png" />
-        <link rel="icon" type="image/png" href="./icon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={`${poppins.className} antialiased relative`}>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5KXQXRR2');`,
+          }}
+        />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KXQXRR2"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <div className="bg-texture absolute inset-0 top-0 left-0 w-full h-full bg-repeat-y lg:bg-cover opacity-20"></div>
         {children}
-        <script
-          async
-          type="text/javascript"
+        <Script
+          strategy="afterInteractive"
           src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=RMPKqS"
-        ></script>
-        <script
-          async
-          type="text/javascript"
+        />
+        <Script
+          strategy="afterInteractive"
           src="https://assets.calendly.com/assets/external/widget.js"
-        ></script>
+        />
+        <GoogleAnalytics gaId="G-WNLWD75BW9" />
       </body>
-      <GoogleAnalytics gaId="G-WNLWD75BW9" />
     </html>
   );
 }
