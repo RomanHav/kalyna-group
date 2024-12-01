845271e3e498a6f18f24767c4bb2c36b46f44f0d
RomanHav <roman14082004@gmail.com>
2024-12-01 20:51:01 +0200

    start policy

---
+++ src/app/privacy-policies/page.tsx
@@ -0,0 +1,27 @@
+const PrivacyPolicy = () => {
+  return (
+      <div className="text-white px-12 py-10 relative z-10">
+        <h2 className="text-[24px] md:text-[36px] text-center mb-7">
+          Privacy Policy for Kalyna Group
+        </h2>
+        <p className="mb-3 text-lg">
+          This Privacy Policy describes how Kalyna Group (the &quot;Site&ldquo;,
+          &quot;we&ldquo;, &quot;us&ldquo;, or &quot;our&ldquo;) collects, uses,
+          and discloses your personal information when you visit, use our
+          services, or communicate with us (collectively, the
+          &quot;Services&ldquo;). For purposes of this Privacy Policy,
+          &quot;you&ldquo; and &quot;your&ldquo; means you as the user of the
+          Services, whether you are a customer or a website visitor.
+        </p>
+        <p className="mb-3 text-lg font-semibold underline decoration-solid">
+          Please read this Privacy Policy carefully. By using and accessing any
+          of the Services, you agree to the collection, use, and disclosure of
+          your information as described in this Privacy Policy. If you do not
+          agree to this Privacy Policy, please do not use or access any of the
+          Services.
+        </p>
+      </div>
+  );
+};
+
+export default PrivacyPolicy;
