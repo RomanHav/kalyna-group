9c6786007015122efe97751e681a3f2fdbe17dab
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-12-01 19:55:15 +0100

    add pages for services

---
+++ src/app/services/page.tsx
@@ -0,0 +1,10 @@
+import Title from "@/app/components/Title";
+
+const ServicesPage: React.FC = ()=> {
+    return (
+        <div>
+            <Title title={'Services'}/>
+        </div>
+    );
+};
+export default ServicesPage;
