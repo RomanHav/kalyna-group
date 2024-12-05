3eeddaf9a9c91cc30ecc6c953fec71aa5def7526
Oleksandr Kalenichenko <62474613+sashakter@users.noreply.github.com>
2024-12-05 21:31:42 +0200

    init new version of Benefits.tsx (#37)
    Co-authored-by: sashakter <149799726+alexsnowll@users.noreply.github.com>

--- src/app/components/Benefits.tsx
+++ src/app/components/Benefits.tsx
@@ -1,9 +1,12 @@
+'use client';
+import React, { useRef } from 'react';
 import BenefitsPart from './BenefitsPart';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/keyboard';
+import Image from 'next/image';
 
 interface BenefitsInfo {
   id?: number;
@@ -18,49 +21,71 @@ interface BenefitsProps {
 }
 
 const Benefits: React.FC<BenefitsProps> = ({ info }) => {
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
+
   return (
     <div className="flex flex-col my-8 relative gap-10 lg:my-16">
-      <div>
-        <Swiper
-          modules={[Navigation, Keyboard]}
-          slidesPerView={'auto'}
-          breakpoints={{
-            375: {
-              slidesPerView: 1,
-              spaceBetween: 32,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 8,
-            },
-            1440: {
-              slidesPerView: 1,
-            },
-          }}
-          loop={false}
-          navigation
-          keyboard={{
-            enabled: true,
-            onlyInViewport: false,
-          }}
-          grabCursor={true}
-          speed={800}
-        >
-          {info.map(infopart => {
-            return (
-              <SwiperSlide key={infopart.id}>
-                <BenefitsPart
-                  id={infopart.id}
-                  src={infopart.src}
-                  numberCard={infopart.numberCard}
-                  title={infopart.title}
-                  description={infopart.description}
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+      {/* Navigation Buttons */}
+      <div className="swiper-navigation">
+        <button ref={prevRef} className="custom-swiper-button-prev">
+          <Image
+            src={'/benefits-arrow-prev.svg'}
+            alt={'prev'}
+            width={262}
+            height={78}
+          />
+        </button>
+        <button ref={nextRef} className="custom-swiper-button-next">
+          <Image
+            src={'/benefits-arrow-next.svg'}
+            alt={'next'}
+            width={262}
+            height={78}
+          />
+        </button>
       </div>
+
+      <Swiper
+        modules={[Navigation, Keyboard]}
+        slidesPerView={'auto'}
+        breakpoints={{
+          375: {
+            slidesPerView: 1,
+            spaceBetween: 32,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 8,
+          },
+          1440: {
+            slidesPerView: 1,
+          },
+        }}
+        loop={false}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        keyboard={{
+          enabled: true,
+          onlyInViewport: false,
+        }}
+        grabCursor={true}
+        speed={800}
+      >
+        {info.map(infopart => (
+          <SwiperSlide key={infopart.id}>
+            <BenefitsPart
+              id={infopart.id}
+              src={infopart.src}
+              numberCard={infopart.numberCard}
+              title={infopart.title}
+              description={infopart.description}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
