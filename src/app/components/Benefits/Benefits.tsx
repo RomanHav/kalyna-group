91ff8c96662bcb3a807de56e670aea9a6799433c
Roman_Havrylov <146737481+RomanHav@users.noreply.github.com>
2024-12-08 00:08:13 +0200

    continue benefits (#38)

--- src/app/components/Benefits/Benefits.tsx
+++ src/app/components/Benefits/Benefits.tsx
@@ -1,12 +1,13 @@
 'use client';
-import React, { useRef } from 'react';
-import BenefitsPart from './BenefitsPart';
+import React, { useRef, useEffect } from 'react';
+import BenefitsPart from '../BenefitsPart/BenefitsPart';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/keyboard';
 import Image from 'next/image';
+import css from './Benefits.module.css'
 
 interface BenefitsInfo {
   id?: number;
@@ -24,23 +25,36 @@ const Benefits: React.FC<BenefitsProps> = ({ info }) => {
   const prevRef = useRef<HTMLButtonElement>(null);
   const nextRef = useRef<HTMLButtonElement>(null);
 
+  useEffect(() => {
+    if (prevRef.current && nextRef.current) {
+    }
+  }, []);
+
   return (
-    <div className="flex flex-col my-8 relative gap-10 lg:my-16">
-      {/* Navigation Buttons */}
-      <div className="swiper-navigation">
-        <button ref={prevRef} className="custom-swiper-button-prev">
+    <div className="relative">
+      {/* Навігаційні кнопки */}
+      <div className="swiper-navigation flex flex-col absolute z-20 bottom-20 left-0 right-0 justify-center items-center px-4">
+        <button
+          ref={prevRef}
+          className={css.customPrev}
+          aria-label="Previous slide"
+        >
           <Image
             src={'/benefits-arrow-prev.svg'}
             alt={'prev'}
-            width={262}
+            width={190}
             height={78}
           />
         </button>
-        <button ref={nextRef} className="custom-swiper-button-next">
+        <button
+          ref={nextRef}
+          className={css.customNext}
+          aria-label="Next slide"
+        >
           <Image
             src={'/benefits-arrow-next.svg'}
             alt={'next'}
-            width={262}
+            width={190}
             height={78}
           />
         </button>
@@ -48,21 +62,8 @@ const Benefits: React.FC<BenefitsProps> = ({ info }) => {
 
       <Swiper
         modules={[Navigation, Keyboard]}
-        slidesPerView={'auto'}
-        breakpoints={{
-          375: {
-            slidesPerView: 1,
-            spaceBetween: 32,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 8,
-          },
-          1440: {
-            slidesPerView: 1,
-          },
-        }}
-        loop={false}
+        slidesPerView={1} // Один слайд на екран
+        loop={true}
         navigation={{
           prevEl: prevRef.current,
           nextEl: nextRef.current,
@@ -73,9 +74,10 @@ const Benefits: React.FC<BenefitsProps> = ({ info }) => {
         }}
         grabCursor={true}
         speed={800}
+        spaceBetween={0}
       >
-        {info.map(infopart => (
-          <SwiperSlide key={infopart.id}>
+        {info.map((infopart) => (
+          <SwiperSlide key={infopart.id} className="!h-[700px] flex items-center justify-center">
             <BenefitsPart
               id={infopart.id}
               src={infopart.src}
