5b9b35419f0c41318233a6cb664bee782232d265
sashakter <149799726+alexsnowll@users.noreply.github.com>
2024-12-08 04:35:18 +0100

    finally? Benefits.tsx globals.css

--- src/app/components/Benefits/Benefits.tsx
+++ src/app/components/Benefits/Benefits.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import BenefitsPart from '../BenefitsPart/BenefitsPart';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Keyboard } from 'swiper/modules';
@@ -7,8 +7,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/keyboard';
 import Image from 'next/image';
-import css from './Benefits.module.css'
-import Title from "@/app/components/Title";
+import css from './Benefits.module.css';
+import Title from '@/app/components/Title';
 
 interface BenefitsInfo {
   id?: number;
@@ -26,86 +26,97 @@ const Benefits: React.FC<BenefitsProps> = ({ info }) => {
   const prevRef = useRef<HTMLButtonElement>(null);
   const nextRef = useRef<HTMLButtonElement>(null);
 
-  useEffect(() => {
-    if (prevRef.current && nextRef.current) {
-    }
-  }, [prevRef, nextRef]);
-
   return (
-      <div className={``}>
-      <Title title={'Benefits'} className={'lg:px-24 px-[45px]'}/>
-    <div className={css.mainContainer}>
-      <div className={css.third}></div>
-      {/* Навігаційні кнопки */}
-      <div className="absolute bottom-40  swiper-navigation flex flex-col  z-40 justify-center items-center px-4">
-        <button
-          ref={prevRef}
-          className={`${css.customPrev}`}
-          aria-label="Previous slide"
+    <div className={``}>
+      <Title title={'Benefits'} className={'lg:px-24 px-[45px]'} />
+      <div className={css.mainContainer}>
+        <div className={css.third}></div>
+        {/* Навігаційні кнопки */}
+
+        <Swiper
+          modules={[Navigation, Keyboard]}
+          slidesPerView={1}
+          keyboard={{
+            enabled: true,
+            onlyInViewport: false,
+          }}
+          onBeforeInit={(swiper) => {
+            if (typeof swiper.params.navigation === "object") {
+              swiper.params.navigation.prevEl = prevRef.current;
+              swiper.params.navigation.nextEl = nextRef.current;
+            }
+          }}
+          onInit={swiper => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-expect-error
+            swiper.params.navigation.prevEl = prevRef.current;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-expect-error
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.update();
+          }}
+          navigation={{
+            prevEl: prevRef.current ? prevRef.current : undefined,
+            nextEl: nextRef.current ? nextRef.current : undefined,
+          }}
+          speed={800}
+          spaceBetween={0}
         >
-          <Image
-            src={'/benefits-arrow-prev.svg'}
-            alt={'prev'}
-            width={190}
-            height={78}
-          />
-          <span
-            className={`absolute top-0 right-0 font-light uppercase tracking-wider`}
-          >
-            prev
-          </span>
-        </button>
-        <button
-          ref={nextRef}
-          className={css.customNext}
-          aria-label="Next slide"
-        >
-          <Image
-            src={'/benefits-arrow-next.svg'}
-            alt={'next'}
-            width={190}
-            height={78}
-          />
-          <span
-            className={`absolute top-0 left-0 font-light uppercase tracking-wider`}
-          >
-            next
-          </span>
-        </button>
+          {info.map(infopart => (
+            <SwiperSlide
+              key={infopart.id}
+              className="!h-[700px] flex items-center justify-center"
+            >
+              <BenefitsPart
+                id={infopart.id}
+                src={infopart.src}
+                numberCard={infopart.numberCard}
+                title={infopart.title}
+                description={infopart.description}
+              />
+            </SwiperSlide>
+          ))}
+          <div className={`relative w-full flex justify-center`}>
+          <div className="absolute bottom-40 swiper-navigation flex flex-col z-40 justify-center items-center px-4">
+            <button
+              ref={prevRef}
+              className={`${css.customPrev}`}
+              aria-label="Previous slide"
+            >
+              <Image
+                src={'/benefits-arrow-prev.svg'}
+                alt={'prev'}
+                width={190}
+                height={78}
+              />
+              <span
+                className={`absolute top-0 right-0 font-light uppercase tracking-wider`}
+              >
+                prev
+              </span>
+            </button>
+            <button
+              ref={nextRef}
+              className={css.customNext}
+              aria-label="Next slide"
+            >
+              <Image
+                src={'/benefits-arrow-next.svg'}
+                alt={'next'}
+                width={190}
+                height={78}
+              />
+              <span
+                className={`absolute top-0 left-0 font-light uppercase tracking-wider`}
+              >
+                next
+              </span>
+            </button>
+          </div>
+          </div>
+        </Swiper>
       </div>
-
-      <Swiper
-        modules={[Navigation, Keyboard]}
-        slidesPerView={1}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-
-        }}
-        keyboard={{
-          enabled: true,
-          onlyInViewport: false,
-        }}
-        speed={800}
-        spaceBetween={0}
-      >
-        {info.map(infopart => (
-          <SwiperSlide
-            key={infopart.id}
-            className="!h-[700px] flex items-center justify-center"
-          >
-            <BenefitsPart
-              id={infopart.id}
-              src={infopart.src}
-              numberCard={infopart.numberCard}
-              title={infopart.title}
-              description={infopart.description}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
     </div>
-      </div>
   );
 };
 
