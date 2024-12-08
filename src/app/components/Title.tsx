bc06d033e4605004ac73e2457800e355ee8e69b1
Oleksandr Kalenichenko <62474613+sashakter@users.noreply.github.com>
2024-12-08 03:08:26 +0200

    fix Benefits.tsx Benefits.module.css for desktop (#40)
    Co-authored-by: sashakter <149799726+alexsnowll@users.noreply.github.com>

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -1,19 +1,24 @@
 interface TitleProps {
   title: string;
   id?: string;
+  className?: string;
 }
 
-const Title: React.FC<TitleProps> = ({ title, id }) => {
+const Title: React.FC<TitleProps> = ({ title, id, className }) => {
     return (
-    <div className="flex justify-between items-center text-white after:content-[''] after:w-10 after:md:w-40 after:bg-white after:float-right after:h-[2px]">
-      <h2
-        id={id ? id : undefined}
-        className="uppercase max-w-52 md:max-w-fit text-[32px] md:text-[48px]"
+      <div
+        className={
+          `flex justify-between items-center text-white after:content-[''] after:w-10 after:md:w-40 after:bg-white after:float-right after:h-[2px] ${className}`
+        }
       >
-        {title}
-      </h2>
-    </div>
-  );
+        <h2
+          id={id ? id : undefined}
+          className="uppercase max-w-52 md:max-w-fit text-[32px] md:text-[48px]"
+        >
+          {title}
+        </h2>
+      </div>
+    );
 };
 
 export default Title;
