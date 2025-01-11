00507577cab6052773f7088712c1d2222bcdde84
Roman_Havrylov <146737481+RomanHav@users.noreply.github.com>
2025-01-11 23:25:14 +0200

    start crm (#65)

--- src/app/crm-system/page.tsx
+++ src/app/crm-system/page.tsx
@@ -1,5 +1,39 @@
-const CrmSystem = () => { 
-    return(<div>Hello</div>);
-}
+import CrmImage from '../components/CrmImage';
+import HeaderService from '../components/HeaderService';
+import offer from '../../../whatWeOfferCrm.json';
+import WhatWeOffer from '../components/WhatWeOffer/WhatWeOffer';
+import WhyServicePage from '../components/WhyServicePage';
+import whyCrmObject from '../../../whyCrm.json';
+import Faq from '../components/Faq/Faq';
+import crmFaq from '../../../crmFaq.json';
+
+const CrmSystem = () => {
+  const faqCard = {
+    title: 'CRM System',
+    src: '/crm.png',
+    price: '$3200',
+    terms: '3-4 weeks',
+  };
+  const headerText = {
+    title: 'Click, Connect, Convert, Complete.',
+    description: 'Your Ideal CRM Solution Awaits You',
+  };
+  const whyCrm = {
+    title: 'Why Your Business Needs a CRM System?',
+    description:
+      'A well-integrated CRM system is crucial for managing customer relationships and streamlining business processes. Here’s what you gain by working with us.',
+  };
+  return (
+    <div className="relative">
+      <HeaderService headerText={headerText} />
+      <CrmImage />
+      <div className={'relative flex flex-col justify-center gap-20'}>
+        <WhatWeOffer offer={offer} />
+        <WhyServicePage whyService={whyCrm} whyServiceObject={whyCrmObject} />
+        <Faq faqCard={faqCard} commerceFaq={crmFaq} />
+      </div>
+    </div>
+  );
+};
 
 export default CrmSystem;
