e8b5a02509d741f1ec0679ef4c838a5ccb5361fe
sashakter <149799726+alexsnowll@users.noreply.github.com>
2025-01-12 22:49:38 +0100

    3d section

--- src/app/3d-design/page.tsx
+++ src/app/3d-design/page.tsx
@@ -1,11 +1,14 @@
 import HeaderService from '../components/HeaderService';
-import ThreeDSystemImage from '../components/ThreeDSystemImage';
+// import ThreeDSystemImage from '../components/ThreeDSystemImage';
 import offer from '../../../whatWeOfferThreeD.json';
 import threeFaq from '../../../threeFaq.json';
 import WhatWeOffer from '../components/WhatWeOffer/WhatWeOffer';
 import OurExpertise from '../components/OurExpertise/OurExpertise';
 import MetaCossacs from '../components/MetaCossacs';
 import Faq from '../components/Faq/Faq';
+import Spline from '@splinetool/react-spline/next';
+import Image from 'next/image';
+import React from 'react';
 
 const ThreeDSystem = () => {
   const faqCard = {
@@ -21,7 +24,28 @@ const ThreeDSystem = () => {
   return (
     <div className="relative">
       <HeaderService headerText={headerText} />
-      <ThreeDSystemImage />
+      <div className={'relative'}>
+        <span
+          className={
+            'absolute z-10 left-32 bottom-28 font-bold text-[220px] text-white'
+          }
+        >
+          3D
+        </span>
+        <div className={'absolute z-[2] left-40 -top-[60rem] flex scale-75'}>
+          <Spline
+            style={{ maxHeight: '1000px' }}
+            scene="https://prod.spline.design/93F01KRNQCu4r7oO/scene.splinecode"
+          />
+        </div>
+        <Image
+          className={'absolute right-20 w-[740px] bottom-[33rem] '}
+          src={'/design.svg'}
+          alt={'3d'}
+          width={740}
+          height={220}
+        />
+      </div>
       <div className={'relative flex flex-col justify-center gap-20'}>
         <WhatWeOffer offer={offer} />
         <OurExpertise>
