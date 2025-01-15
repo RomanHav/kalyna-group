5e9964fba1da4188e99abfa1206c074eedcb47f3
sashakter <149799726+alexsnowll@users.noreply.github.com>
2025-01-15 02:52:44 +0100

    edit crm and 3d, optimize images

--- src/app/3d-design/page.tsx
+++ src/app/3d-design/page.tsx
@@ -1,14 +1,15 @@
-import HeaderService from '../components/HeaderService';
-// import ThreeDSystemImage from '../components/ThreeDSystemImage';
+const HeaderService = lazy(() => import('../components/HeaderService'));
 import offer from '../../../whatWeOfferThreeD.json';
 import threeFaq from '../../../threeFaq.json';
-import WhatWeOffer from '../components/WhatWeOffer/WhatWeOffer';
-import OurExpertise from '../components/OurExpertise/OurExpertise';
-import MetaCossacs from '../components/MetaCossacs';
-import Faq from '../components/Faq/Faq';
+const WhatWeOffer = lazy(() => import('../components/WhatWeOffer/WhatWeOffer'));
+const OurExpertise = lazy(
+  () => import('../components/OurExpertise/OurExpertise')
+);
+const MetaCossacs = lazy(() => import('../components/MetaCossacs'));
+const Faq = lazy(() => import('../components/Faq/Faq'));
 import Spline from '@splinetool/react-spline/next';
 import Image from 'next/image';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 const ThreeDSystem = () => {
   const faqCard = {
@@ -22,38 +23,40 @@ const ThreeDSystem = () => {
     description: 'Your Perfect 3D Design Awaits you',
   };
   return (
-    <div className="relative">
-      <HeaderService headerText={headerText} />
-      <div className={'relative'}>
-        <span
-          className={
-            'absolute z-10 left-32 bottom-28 font-bold text-[220px] text-white'
-          }
-        >
-          3D
-        </span>
-        <div className={'absolute z-[2] left-40 -top-[60rem] flex scale-75'}>
-          <Spline
-            style={{ maxHeight: '1000px' }}
-            scene="https://prod.spline.design/93F01KRNQCu4r7oO/scene.splinecode"
+    <Suspense fallback={<div>Loading</div>}>
+      <div className="relative">
+        <HeaderService headerText={headerText} />
+        <div className={'relative'}>
+          <span
+            className={
+              'absolute z-10 left-32 bottom-28 font-bold text-[220px] text-white'
+            }
+          >
+            3D
+          </span>
+          <div className={'absolute z-[2] left-40 -top-[60rem] flex scale-75'}>
+            <Spline
+              style={{ maxHeight: '1000px' }}
+              scene="https://prod.spline.design/93F01KRNQCu4r7oO/scene.splinecode"
+            />s
+          </div>
+          <Image
+            className={'absolute right-20 w-[740px] bottom-[33rem] '}
+            src={'/design.svg'}
+            alt={'3d'}
+            width={740}
+            height={220}
           />
         </div>
-        <Image
-          className={'absolute right-20 w-[740px] bottom-[33rem] '}
-          src={'/design.svg'}
-          alt={'3d'}
-          width={740}
-          height={220}
-        />
+        <div className={'relative flex flex-col justify-center gap-20'}>
+          <WhatWeOffer offer={offer} />
+          <OurExpertise>
+            <MetaCossacs />
+          </OurExpertise>
+          <Faq faqCard={faqCard} commerceFaq={threeFaq} />
+        </div>
       </div>
-      <div className={'relative flex flex-col justify-center gap-20'}>
-        <WhatWeOffer offer={offer} />
-        <OurExpertise>
-          <MetaCossacs />
-        </OurExpertise>
-        <Faq faqCard={faqCard} commerceFaq={threeFaq} />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
