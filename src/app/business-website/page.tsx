9ed421416d96b3496d00649357a1563b9a135cfd
RomanHav <roman14082004@gmail.com>
2025-01-19 17:45:21 +0200

    finish business

--- src/app/business-website/page.tsx
+++ src/app/business-website/page.tsx
@@ -1,5 +1,38 @@
-const BusinessWebsite = () => { 
-    return(<div>Hello</div>);
-}
+import BusinessImage from '../components/BusinessImage';
+import HeaderService from '../components/HeaderService';
+import WhatWeOffer from '../components/WhatWeOffer/WhatWeOffer';
+import offer from '../../../whatWeOffer/whatWeOfferBusiness.json';
+import OurExpertise from '../components/OurExpertise/OurExpertise';
+import Alcotrade from '../components/Alcotrade';
+import Faq from '../components/Faq/Faq';
+import businessFaq from '../../../faq/businessFaq.json';
+
+const BusinessWebsite = () => {
+  const faqCard = {
+    title: 'Business Website',
+    src: '/business.webp',
+    price: '$4000',
+    terms: '4-8 weeks',
+  };
+  const headerText = {
+    title: 'Innovate, Inspire, Impress, Succeed.',
+    description: 'Your Perfect Business Website Awaits You',
+  };
+  return (
+    <div>
+      <div className="relative mb-20">
+        <HeaderService headerText={headerText} />
+        <BusinessImage />
+        <div className={'relative flex flex-col justify-center gap-20'}>
+          <WhatWeOffer offer={offer} />
+          <OurExpertise>
+            <Alcotrade />
+          </OurExpertise>
+          <Faq faqCard={faqCard} commerceFaq={businessFaq} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default BusinessWebsite;
---
+++ src/app/components/Alcotrade.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+
+const Alcotrade = () => {
+  const skills = [
+    'Next.js',
+    'Figma',
+    'Authorization',
+    'API Integration',
+    'Database Management',
+  ];
+  return (
+    <div className="flex items-center justify-between">
+      <div className="w-1/2">
+        <div className="relative flex justify-center items-center rounded-lg w-[538px] h-[354px] bg-businessExpertise bg-cover bg-center">
+          <div className="absolute w-full h-full bg-[#263238] opacity-70 rounded-lg"></div>
+          <div className="absolute flex justify-center items-center z-10 w-full h-full">
+            <span className="uppercase text-5xl border-b pb-2 border-white font-light text-white">
+              Alcotrade
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6 text-white w-1/2">
+        <div className="flex justify-between items-center">
+          <h4 className="text-4xl uppercase">Alcotrade</h4>
+          <a href={'https://www.alcotrade.com.ua/'} target="_blanc">
+            <div className="flex justify-center items-center border border-white rounded-full rotate-[135deg] w-11 h-11">
+              <Image
+                src={'/arrow-cases.svg'}
+                alt="Wood Minded Button"
+                width={18}
+                height={26}
+              />
+            </div>
+          </a>
+        </div>
+        <p className="text-lg w-11/12">
+          Our team developed Alcotrade, a web application for alcohol export,
+          aimed at fostering collaboration. By integrating animation libraries,
+          we created an engaging user experience. The platform features strict
+          typing for reliability and advanced authentication for secure
+          partnerships in the alcohol export industry.
+        </p>
+        <ul className="flex flex-wrap gap-4 w-9/12">
+          {skills.map((skill, index) => {
+            return (
+              <li
+                key={index}
+                className="text-base text-white/70 rounded-full border border-white/70 px-6 py-2"
+              >
+                {skill}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Alcotrade;
