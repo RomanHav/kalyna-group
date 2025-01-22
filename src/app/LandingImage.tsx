b2bd4f614e5463586ed2b8b3325ebf9a2e917ec1
RomanHav <roman14082004@gmail.com>
2025-01-22 23:07:16 +0200

    finish business and landing adaptive

--- src/app/LandingImage.tsx
+++ src/app/LandingImage.tsx
@@ -1,63 +1,26 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { Parallax } from 'react-scroll-parallax';
+import LandingRunningLine from './components/LandingRunningLine';
 
 const LandingImage = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    setIsMobile(window.innerWidth <= 768);
-  }, []);
-
   return (
-    <div className="absolute top-[27rem] lg:top-[30rem] z-10 w-full flex flex-col justify-center items-center text-white">
-      <Image
-        className="w-[350px] md:w-[550px] lg:w-[650px] xl:w-[836px]"
-        src={'/macbook.png'}
-        alt="Phone Image"
-        width={1338}
-        height={806}
-      />
-      <Parallax
-        className={
-          'absolute bottom-12 right-0 md:right-[9rem] lg:bottom-24 lg:right-[8rem] xl:bottom-28 xl:right-[14rem] min-[1440px]:right-[19rem] tracking-[0.5em] flex justify-center items-center w-full h-full'
-        }
-        speed={isMobile ? 0 : 5}
-      >
-        <div className="text-[60px] md:text-[90px] lg:text-[140px] xl:text-[160px] font-bold flex items-center lg:gap-3">
-          <span className="text-white uppercase">
-            Lan<span className="md:hidden">ding</span>
-          </span>
-          <Image
-            className="hidden md:block md:w-[237px] lg:w-[368px] xl:w-[421px]"
-            src={'/landing.svg'}
-            alt="Landing Image"
-            width={421}
-            height={240}
-          />
+    <div className="absolute top-[24rem] md:top-[25rem] lg:top-[29rem] xl:top-[26rem] min-[1440px]:top-[25rem] z-10 w-full flex flex-col justify-center items-center text-white">
+      <div className="relative z-20 lg:left-[5%] flex justify-center">
+        <Image
+          className="w-80 md:w-[680px] lg:w-[750px] xl:w-[960px]"
+          src={'/landing-read.webp'}
+          alt="Landing Image"
+          width={4082}
+          height={3622}
+        />
+      </div>
+      <div className='relative w-[150%] bottom-[12rem] md:bottom-[25rem] lg:bottom-[27rem] xl:bottom-[34rem] z-10'>
+        <div className="absolute z-20 -rotate-[20deg] w-full">
+          <LandingRunningLine />
         </div>
-      </Parallax>
-      <Parallax
-        className={
-          'absolute -bottom-5 left-0 md:-bottom-12 md:left-[14.5rem] lg:-bottom-16 lg:left-[16rem] xl:left-[21.5rem] tracking-[0.5em] flex justify-center items-center w-full h-full'
-        }
-        speed={isMobile ? 0 : 5}
-      >
-        <div className="text-[60px] md:text-[90px] lg:text-[160px] font-bold flex items-center lg:gap-3">
-          <Image
-            className="hidden md:block md:w-[126px] lg:w-[223px]"
-            src={'/page.svg'}
-            alt="Landing Image"
-            width={223}
-            height={240}
-          />
-          <span className="text-white uppercase">
-            <span className="md:hidden">Pa</span>ge
-          </span>
-        </div>
-      </Parallax>
+        <div className="absolute z-10 left-8 rotate-[20deg] w-full h-8 md:h-[52px] lg:h-[56px] xl:h-[76px] bg-gradient-to-b from-[#9CFF95] to-[#7DEBA9] opacity-30"></div>
+      </div>
     </div>
   );
 };
--- src/app/components/BusinessImage.tsx
+++ src/app/components/BusinessImage.tsx
@@ -2,20 +2,20 @@ import Image from 'next/image';
 
 const BusinessImage = () => {
   return (
-    <div className="w-full h-[560px] bg-businessImage absolute top-[28rem] z-10 flex flex-col justify-center gap-6 items-center text-white">
-      <div className="flex items-center gap-20">
-        <span className="uppercase text-[128px] font-bold">Business</span>
+    <div className="w-full h-[146px] md:h-[300px] lg:h-[400px] xl:h-[560px] bg-businessImage bg-cover absolute top-[28rem] lg:top-[32rem] xl:top-[28rem] z-10 flex flex-col justify-center gap-2 lg:gap-6 items-center text-white">
+      <div className="flex items-center gap-1 md:gap-8 lg:gap-20">
+        <span className="uppercase text-5xl md:text-[80px] lg:text-[128px] font-bold">Business</span>
         <Image
-          className='w-[200px]'
+          className='w-[100px] md:w-[150px] lg:w-[200px]'
           src={'/growth.webp'}
           alt="Arrow Image"
           width={640}
           height={640}
         />
       </div>
-      <div className="flex items-center gap-20 pl-[10%]">
+      <div className="flex items-center gap-1 md:gap-8 lg:gap-20 xl:pl-[10%]">
         <Image
-          className='w-[200px]'
+          className='w-[100px] md:w-[150px] lg:w-[200px]'
           src={'/people.webp'}
           alt="People Image"
           width={640}
@@ -23,6 +23,7 @@ const BusinessImage = () => {
         />
         <div className="w-full flex flex-col gap-3">
           <Image
+          className='w-[240px] md:w-[380px] lg:w-[573px]'
             src={'/website.svg'}
             alt="Business Logo Image"
             width={573}
