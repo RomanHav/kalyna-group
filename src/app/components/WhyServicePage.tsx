c5dd5405267de7a883927f0a4247f4673b0d897e
sashakter <149799726+alexsnowll@users.noreply.github.com>
2025-01-26 16:24:06 +0100

    why crm fix

--- src/app/components/WhyServicePage.tsx
+++ src/app/components/WhyServicePage.tsx
@@ -1,5 +1,5 @@
-import WhyServicePagePart from './WhyServicePagePart';
 import React from 'react';
+import WhyServicePagePart from './WhyServicePagePart';
 
 interface WhyServiceProps {
   title: string;
@@ -16,7 +16,7 @@ interface WhyServiceObjectProps {
 
 interface WhyServicePageProps {
   whyService: WhyServiceProps;
-  whyServiceObject: WhyServiceObjectProps[];
+  whyServiceObject: WhyServiceObjectProps[][];
 }
 
 const WhyServicePage: React.FC<WhyServicePageProps> = ({
@@ -25,6 +25,7 @@ const WhyServicePage: React.FC<WhyServicePageProps> = ({
 }) => {
   return (
     <div className="flex flex-col gap-16 lg:gap-0 lg:flex-row justify-between px-0 lg:px-20 xl:px-36">
+      {/* Title and Description */}
       <div className="w-full lg:w-1/2 flex flex-col gap-8 md:gap-16 px-10 md:px-20 lg:px-0">
         <h3 className="text-4xl md:text-5xl leading-[1.25] lg:leading-[1.3] xl:leading-[1.25] lg:w-[420px] font-medium text-[#38EF7D]">
           {whyService.title}
@@ -33,28 +34,28 @@ const WhyServicePage: React.FC<WhyServicePageProps> = ({
           {whyService.description}
         </p>
       </div>
-      <ul className="relative w-full lg:w-1/2 flex flex-wrap justify-end items-center text-white">
-        <li
-          className={
-            'hidden lg:block absolute w-[1px] h-full bg-gradient-to-b from-transparent via-white to-transparent left-0 top-0'
-          }
-        ></li>
-        {whyServiceObject.map(part => {
-          return (
-            <li
-              key={part.id}
-              className={`flex ${part.id === 1 || part.id === 4 || part.id === 5 ? 'items-end' : ''} w-1/2 lg:min-h-[260px]`}
-            >
-              <WhyServicePagePart
-                id={part.id}
-                title={part.title}
-                description={part.description}
-                number={part.number}
-                src={part.src}
-              />
-            </li>
-          );
-        })}
+
+      {/* Render 3 <ul> elements */}
+      <ul className="relative w-full lg:w-1/2 list-none flex flex-wrap justify-end items-center text-white">
+        {[0, 1, 2].map(groupIndex => (
+          <ul
+            key={groupIndex}
+            className={`relative flex w-full list-none ${groupIndex !== 2 && 'after:absolute after:h-[1px] after:w-full after:bg-gradient-to-r after:from-transparent after:via-white after:to-transparent after:left-0 after:bottom-0'}`}
+          >
+            {whyServiceObject[groupIndex]?.map(item => (
+              <li key={item.id} className={'w-1/2'}>
+                <WhyServicePagePart
+                  id={item.id}
+                  title={item.title}
+                  description={item.description}
+                  number={item.number}
+                  src={item.src}
+                />
+              </li>
+            ))}
+          </ul>
+        ))}
+
         <li
           className={
             'absolute w-[1px] h-full bg-gradient-to-b from-transparent via-white to-transparent left-1/2 top-0'
@@ -62,19 +63,19 @@ const WhyServicePage: React.FC<WhyServicePageProps> = ({
         ></li>
         <li
           className={
-            'hidden lg:block absolute w-[1px] h-full bg-gradient-to-b from-transparent via-white to-transparent right-0 top-0'
+            'absolute w-[1px] h-full bg-gradient-to-b from-transparent via-white to-transparent left-0 top-0'
           }
         ></li>
         <li
           className={
-            'absolute h-[1px] w-full bg-gradient-to-r from-transparent via-white to-transparent left-0 top-1/3'
+            'absolute w-[1px] h-full bg-gradient-to-b from-transparent via-white to-transparent right-0 top-0'
           }
         ></li>
-        <li
-          className={
-            'absolute h-[1px] w-full bg-gradient-to-r from-transparent via-white to-transparent left-0 top-2/3'
-          }
-        ></li>
+        {/*<li*/}
+        {/*  className={*/}
+        {/*    'absolute h-[1px] w-full bg-gradient-to-r from-transparent via-white to-transparent left-0 top-1/3'*/}
+        {/*  }*/}
+        {/*></li>*/}
       </ul>
     </div>
   );
