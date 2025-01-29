eefd36f4b10b30547fdfab46353b8287a436c470
Roman_Havrylov <146737481+RomanHav@users.noreply.github.com>
2025-01-30 00:57:18 +0200

    Merge pull request #95
    * add mobile services

---
+++ src/app/components/MobileServices.tsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectCards } from 'swiper/modules';
+import 'swiper/css/effect-cards';
+
+// Import Swiper styles
+import 'swiper/css';
+import ServicePart from '@/app/components/Services/ServicePart';
+import React from 'react';
+
+interface ServiceInfo {
+  id: number;
+  title: string;
+  src: string;
+  price: string;
+}
+
+interface ServiceProps {
+  info: ServiceInfo[];
+}
+
+const MobileServices: React.FC<ServiceProps> = ({ info }) => {
+  return (
+    <div
+      className={`relative z-10 w-full flex justify-center text-white px-[45px] py-6`}
+    >
+      <Swiper
+        modules={[EffectCards, Autoplay]}
+        className={`w-full`}
+        effect="cards"
+        autoplay={{ delay: 2500 }}
+        speed={1000}
+      >
+        {info.map(infopart => (
+          <SwiperSlide
+            key={infopart.id}
+            className="bg-black/60 backdrop-blur-3xl shadow-customSecond relative flex items-center rounded-3xl"
+          >
+            <div className="absolute w-full h-full rounded-3xl shadow-customThird"></div>
+            <ServicePart
+              id={infopart.id}
+              title={infopart.title}
+              src={infopart.src}
+              price={infopart.price}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MobileServices;
