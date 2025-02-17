c87ad5ff5b4c0fcd893c8e80c02b6a52cd12a968
sashakter <sashakter@gmail.com>
2025-02-17 16:54:15 +0100

    StartingLoader.tsx change config

--- src/app/components/StartingLoader/StartingLoader.tsx
+++ src/app/components/StartingLoader/StartingLoader.tsx
@@ -6,7 +6,7 @@ const StartingLoader = () => {
       className={`${css.loaderContainer} overflow-hidden fixed z-[9998] top-0 left-0 w-full h-screen`}
     >
       <div
-        className={`absolute z-[9999] top-0 left-0 w-full h-screen bg-black/60 transition-all backdrop-blur-md`}
+        className={`absolute z-[9999] top-0 left-0 w-full h-screen bg-black/60 transition-all`}
       >
         <div
           className={`relative w-full h-screen flex justify-center items-center`}
@@ -17,12 +17,13 @@ const StartingLoader = () => {
             loop
             muted
             playsInline
+            preload={'auto'}
           >
-            <source src={'/starting-loader.webm'} type={'video/webm'} />
             <source
               src={'/starting-loader2.mov'}
-              type='video/mp4; codecs="hvc1"'
+              type={'video/mp4; codecs="hvc1"'}
             />
+            <source src={'/starting-loader.webm'} type={'video/webm'} />
           </video>
         </div>
       </div>
