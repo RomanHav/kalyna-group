488cc10a9e6997ffee37e407c4b50f2691d793b8
sashakter <sashakter@gmail.com>
2025-03-02 22:57:56 +0100

    edit cookie style, add svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://kalynagroup.space'),
+  metadataBase: new URL('https://www.kalynagroup.space'),
   title: 'Kalyna Group',
   description: 'Create an effective website for your Business with us',
   openGraph: {
