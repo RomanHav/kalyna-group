c492d4389ac41430b5c5616a17bc91cac8b80a81
sashakter <sashakter@gmail.com>
2025-04-10 11:17:15 +0200

    edit robots.txt

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,13 +1,15 @@
 // /app/robots.txt/route.ts
-import { type MetadataRoute } from 'next';
+export async function GET() {
+  const content = `
+  User-Agent: *
+  Allow: /
+  
+  Sitemap: https://www.kalynagroup.space/sitemap.xml
+    `.trim();
 
-export function GET(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin',
+  return new Response(content, {
+    headers: {
+      'Content-Type': 'text/plain',
     },
-    sitemap: 'https://www.kalynagroup.space/sitemap.xml',
-  };
+  });
 }
